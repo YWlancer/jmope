2978ecff78af06538a5ef30982d7490bb5439271
sithum nissanka <sithumm@gmail.com>
2014-08-11 21:16:41 +0530

    Added gitignore

---
+++ src/main/java/org/ope/jmope/App.java
@@ -0,0 +1,13 @@
+package org.ope.jmope;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
